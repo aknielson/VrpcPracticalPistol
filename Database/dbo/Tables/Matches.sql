861e726581b83a84e2dc2c8462d080e95dbb1fa1
aknielson <aknielson@hotmail.com>
2015-12-15 20:10:31 -0800

    for some resaon git sucks and didn't add my db files

---
+++ Database/dbo/Tables/Matches.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Matches] (
+    [Id]   INT      IDENTITY (1, 1) NOT NULL,
+    [Date] DATETIME NOT NULL,
+    CONSTRAINT [PK_dbo.Matches] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Database/dbo/Tables/Stages.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[Stages] (
+    [Id]                     INT            IDENTITY (1, 1) NOT NULL,
+    [StageName]              NVARCHAR (MAX) NOT NULL,
+    [IncludeInCombinedScore] BIT            NOT NULL,
+    [IsVirginia]             BIT            NOT NULL,
+    [NumberOfStrings]        INT            NOT NULL,
+    [Designer_Id]            INT            NULL,
+    [Match_Id]               INT            NULL,
+    CONSTRAINT [PK_dbo.Stages] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_dbo.Stages_dbo.Matches_Match_Id] FOREIGN KEY ([Match_Id]) REFERENCES [dbo].[Matches] ([Id]),
+    CONSTRAINT [FK_dbo.Stages_dbo.Members_Designer_Id] FOREIGN KEY ([Designer_Id]) REFERENCES [dbo].[Members] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Match_Id]
+    ON [dbo].[Stages]([Match_Id] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Designer_Id]
+    ON [dbo].[Stages]([Designer_Id] ASC);
+
