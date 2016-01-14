dbd5220b3040627eda74275b0d73c1ae57943dc0
aknielson <aknielson@hotmail.com>
2016-01-13 21:51:40 -0800

    populating from tables

--- OutOfTheBoxMvc/Controllers/ScoreController.cs
+++ OutOfTheBoxMvc/Controllers/ScoreController.cs
@@ -15,6 +15,38 @@ namespace OutOfTheBoxMvc.Controllers
         // GET: Score
         public ActionResult Index()
         {
+            var match = db.Matches.OrderByDescending(x => x.Date).ToList().FirstOrDefault();
+            var matchId = match?.Id;
+            var competitors = db.Competitors.Where(x => x.Match_Id == matchId).ToList();
+            
+            if (db.CompetitorStages.Count() == 0 || !db.CompetitorStages.Any(x => x.Match_Id == matchId))
+            {
+                foreach (var competitor in competitors)
+                {
+                    foreach (var stage in db.Stages.Where(x => x.Match_Id == matchId).ToList())
+                    {
+
+                        var competitorStage = new CompetitorStage();
+                        competitorStage.Match_Id = matchId;
+                        competitorStage.Competitor_Id = competitor.Id;
+                        competitorStage.Stage_Id = stage.Id;
+                        //competitorStage.MatchStageTimes = new List<MatchStageTime>(stage.NumberOfStrings);
+                        for(int i = 0; i < stage.NumberOfStrings; i++)
+                        {
+                            db.MatchStageTimes.Add(new MatchStageTime { CompetitorStage = competitorStage });
+                        }
+                        db.CompetitorStages.Add(competitorStage);
+                    }
+
+                }
+                db.SaveChanges();
+            }
+
+            var competitorStageScores = db.CompetitorStages.Where(x => x.Match_Id == matchId).ToList();
+            var competitorStageScoresEmpty = competitorStageScores.Where(x => x.MatchStageTimes.Any(p => p.StageTime != new TimeSpan(0))).ToList();
+
+            var competitorStageScoresFilled = competitorStageScores.Where(x => x.MatchStageTimes.Any(p => p.StageTime == new TimeSpan(0))).ToList();
+            // var competitorStageScoresFilled = competitorStageScores.Where(x => x.MatchStageTimes.Any()).ToList();
             return View();
         }
 
