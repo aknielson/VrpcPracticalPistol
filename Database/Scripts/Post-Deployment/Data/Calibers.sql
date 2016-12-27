5e3542b85489ade3c5abddb9efff7778692d3e5d
Allen Nielson <aknielson@hotmail.com>
2016-12-27 12:16:05 -0800

    added db table population

---
+++ Database/Scripts/Post-Deployment/Data/Calibers.sql
@@ -0,0 +1,21 @@
+﻿
+if not exists (select * from [Calibers])
+begin
+	SET IDENTITY_INSERT [dbo].[Calibers] ON 
+
+	
+	INSERT [dbo].[Calibers] ([Id], [Name], [Description]) VALUES (1, N'9MM', N'9MM')
+	
+	INSERT [dbo].[Calibers] ([Id], [Name], [Description]) VALUES (2, N'45 ACP', N'45 ACP')
+	
+	INSERT [dbo].[Calibers] ([Id], [Name], [Description]) VALUES (3, N'40 SW', N'40 SW')
+	
+	INSERT [dbo].[Calibers] ([Id], [Name], [Description]) VALUES (4, N'38 Special', N'38 Special')
+	
+	INSERT [dbo].[Calibers] ([Id], [Name], [Description]) VALUES (5, N'38 Super', N'38 Super')
+	
+	INSERT [dbo].[Calibers] ([Id], [Name], [Description]) VALUES (6, N'380 ACP', N'380 ACP')
+	
+	SET IDENTITY_INSERT [dbo].[Calibers] OFF
+	
+end
---
+++ Database/Scripts/Post-Deployment/Data/Members.sql
@@ -0,0 +1,19 @@
+﻿if not exists (select * from [Members])
+begin
+	SET IDENTITY_INSERT [dbo].[Members] ON 
+
+	
+	INSERT [dbo].[Members] ([Id], [FirstName], [LastName], [EmailAddress]) VALUES (1, N'Allen', N'Nielson', N'aknielson@hotmail.com')
+	
+	INSERT [dbo].[Members] ([Id], [FirstName], [LastName], [EmailAddress]) VALUES (2, N'Don', N'Yash', N'Don.Yash@example.com')
+	
+	INSERT [dbo].[Members] ([Id], [FirstName], [LastName], [EmailAddress]) VALUES (3, N'Parker', N'Leach', N'pleach@example.com')
+	
+	INSERT [dbo].[Members] ([Id], [FirstName], [LastName], [EmailAddress]) VALUES (4, N'Dave', N'Saxon', N'dsaxon@example.com')
+	
+	INSERT [dbo].[Members] ([Id], [FirstName], [LastName], [EmailAddress]) VALUES (5, N'Tim', N'Hay', N'thay@example.com')
+	
+	INSERT [dbo].[Members] ([Id], [FirstName], [LastName], [EmailAddress]) VALUES (6, N'Joanne', N'Tagemeyer', N'jt@example.com')
+	
+	SET IDENTITY_INSERT [dbo].[Members] OFF
+end
